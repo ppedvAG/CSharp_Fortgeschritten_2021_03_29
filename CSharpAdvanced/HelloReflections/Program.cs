84058e11858b659e69ed5a5e9a0d4f2286fb4f78
Kevin-Winter-Codes <Kevin.Winter.Privat@gmail.com>
2021-03-31 10:40:24 +0200

    Reflections
    PlugIn-Pattern von Microsoft beigefügt

---
+++ CSharpAdvanced/HelloReflections/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Reflection;
+
+namespace HelloReflections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Lade DLL 
+            Assembly geladeneDll = Assembly.LoadFrom("TrumpTaschenrechner.dll");
+
+            //Laden der Klasse
+            Type trumpTaschenrechnerTyp = geladeneDll.GetType("TrumpTaschenrechner.Taschenrechner");
+
+            object tr = Activator.CreateInstance(trumpTaschenrechnerTyp);
+
+            MethodInfo addInfo = trumpTaschenrechnerTyp.GetMethod("Add", new Type[] { typeof(Int32), typeof(Int32) });
+            var result = addInfo.Invoke(tr, new object[] { 6, 4 });
+
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CSharpAdvanced/async_await_sample/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace async_await_sample
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            //Task task = MethodOld();
+            //task.Wait();
+
+            // await Method1();
+
+
+            List<int> resultList = await AsyncWithParameter();
+        }
+
+        public static Task MethodOld()
+        {
+            Task myTask = Task.Run(() =>
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    Console.WriteLine(" Method 1");
+                    // Do something
+                    Task.Delay(100).Wait();
+                }
+            });
+
+            return myTask;
+        }
+
+
+        public static async Task Method1()
+        {
+            await Task.Run(() =>
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    Console.WriteLine(" Method 1");
+                    // Do something
+                    Task.Delay(100).Wait();
+                }
+            });
+        }
+
+
+        public static async Task<List<int>> AsyncWithParameter()
+        {
+            List<int> retList = new List<int>();
+            await Task.Run(() =>
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    retList.Add(i);
+                    // Do something
+                    //Task.Delay(100).Wait();
+                }
+            });
+
+            return retList;
+        }
+
+
+
+
+    }
+}
